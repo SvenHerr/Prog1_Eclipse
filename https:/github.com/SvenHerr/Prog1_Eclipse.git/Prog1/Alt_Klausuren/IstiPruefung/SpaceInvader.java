00d48070d3841965b98fe105ce4c1b6d229a21c4
Sven Herrmann <sven92.h@web.de>
2019-07-17 11:21:52 +0200

    SpaceInvader fertig

--- https:/github.com/SvenHerr/Prog1_Eclipse.git/Prog1/Alt_Klausuren/IstiPruefung/SpaceInvader.java
+++ https:/github.com/SvenHerr/Prog1_Eclipse.git/Prog1/Alt_Klausuren/IstiPruefung/SpaceInvader.java
@@ -1,9 +1,11 @@
 package IstiPruefung;
 
+import java.util.Scanner;
+
 public class SpaceInvader 
 {
 	static String[][] feld = new String[5][8];
-
+	int value;
 	public void initialisiereFeld() 
 	{
 		// obere Reihe mit bösen Aliens befüllen
@@ -22,24 +24,58 @@ public class SpaceInvader
 		}
 		
 		// Raumschiff setzen
-		int value = ((int)(Math.random()*8)); 
+		value = ((int)(Math.random()*8)); 
 		//System.out.println(value);
 		feld[4][value] = "v";
 		
+	}
+	
+	public void bewege(char c) 
+	{
+		if(c == 'a' && value > 0) 
+		{
+			feld[4][value] = " ";
+			value = value -1;
+			feld[4][value] = "v";
+			
+		}
+		else if(c == 'd' && value < 7) 
+		{
+			feld[4][value] = " ";
+			value = value +1;
+			feld[4][value] = "v";
+		}
+	}
+	
+	public String toString()  // Ka ob das so stimmt. Ist vllt zu kompliziert gelößt
+	{
+		String result = "";
+		for (int i = 0; i < feld.length; i++) 
+		{
+			for (int k = 0; k < feld[0].length; k++) 
+			{
+				result = result + feld[i][k];
+			}
+			result = result + "\n";	
+		}
+		return result;
 	}
 
 	public static void main(String[] args) 
 	{	
 		SpaceInvader s = new SpaceInvader();
 		s.initialisiereFeld();
-		for (int i = 0; i < feld.length; i++) 
+		System.out.println(s.toString());
+		Scanner scan = new Scanner(System.in);
+		
+		char c = 'o';
+		do 
 		{
-			for (int k = 0; k < feld[0].length; k++) 
-			{
-				System.out.print(feld[i][k]);
-			}
-			System.out.println();
-			
-		}	
+			c = scan.nextLine().charAt(0);
+			s.bewege(c);
+			System.out.println(s.toString());
+		}
+		while(c != 'x');
+		scan.close();
 	}
 }
